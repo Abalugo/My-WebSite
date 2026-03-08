6181280b5d043c7945fa472e01b2a2b232ef9ca9
Abalugo <mosesabalugo18@gmail.com>
2026-03-08 09:46:03 +0100

    add navbar and body

---
+++ index.js
@@ -0,0 +1,30 @@
+
+let x=5;
+let y=2;
+let z=y+x;
+
+// strings in javascript
+'Abalugo'
+let name='Abalugo';
+let car='Benz';
+
+// Template String
+`bnhnhggjkykkuulujggjgjjh`;
+`He's called "Abalugo"`;
+
+// Number: in javascript u can not make 
+let w="10";
+let j=20;
+let k=w+j;
+
+// inteder: they are whole numbers
+
+// Function: are reuseable code block 
+
+function Chijioke() {
+
+}
+function Hello() {
+
+    
+}
